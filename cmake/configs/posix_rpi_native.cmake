ef08f860dcb88c163613846a01c5351202d268c9
Shawn <Shawn@raspberry.com>
2018-01-27 13:47:08 +0800

    add wiringPi lib to cmake config

--- cmake/configs/posix_rpi_native.cmake
+++ cmake/configs/posix_rpi_native.cmake
@@ -3,5 +3,5 @@ include(configs/posix_rpi_common)
 add_definitions(
 	-D __DF_RPI
 	)
-
+SET(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-lwiringPi")
 set(CMAKE_TOOLCHAIN_FILE ${PX4_SOURCE_DIR}/cmake/toolchains/Toolchain-native.cmake)
